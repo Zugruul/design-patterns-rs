0de683e230a0462f74dd19b2673a90d9cc0d6db9
Leonardo Vieira <leonardo.marcelino@outlook.com>
2021-07-30 17:45:56 -0300

    Update factory example

--- src/factory.rs
+++ src/factory.rs
@@ -1,8 +1,8 @@
 pub trait Factory {
-    type Kind;
+    type Input;
     type Output;
 
-    fn create(kind: Self::Kind) -> Self::Output;
+    fn create(input: Self::Input) -> Self::Output;
 }
 
 #[cfg(test)]
@@ -17,7 +17,7 @@ mod tests {
         Protection
     }
 
-    trait EquipableItem {
+    trait EquipableItem: Any {
         fn get_usage(&self) -> Usage;
         
         fn as_any(&self) -> &dyn Any;
@@ -67,13 +67,14 @@ mod tests {
     }
     
     struct ItemFactory;
+
     impl Factory for ItemFactory {
-        type Kind = Item;
+        type Input = Item;
         type Output = Box<dyn EquipableItem>;
   
-        fn create(kind: Self::Kind) -> Self::Output {
-            match kind {
-                Item::Bow => Box::new(Bow {}),
+        fn create(input: Self::Input) -> Self::Output {
+            match input {
+                Item::Bow => Box::new(Bow {}),  
                 Item::Axe => Box::new(Axe {}),
                 Item::Armor => Box::new(Armor {}),
             }
@@ -84,26 +85,32 @@ mod tests {
     fn it_creates_bow() {
         let item = ItemFactory::create(Item::Bow);
         let possibly_a_bow = item.as_any().downcast_ref::<Bow>();
+        let possibly_an_armor = item.as_any().downcast_ref::<Armor>();
 
         assert_eq!(item.get_usage(), Usage::RangedWeapon);
-        assert_eq!(possibly_a_bow.is_some(), true)
+        assert_eq!(possibly_a_bow.is_some(), true);
+        assert_eq!(possibly_an_armor.is_some(), false);
     }
 
     #[test]
     fn it_creates_axe() {
         let item = ItemFactory::create(Item::Axe);
         let possibly_an_axe = item.as_any().downcast_ref::<Axe>();
+        let possibly_an_armor = item.as_any().downcast_ref::<Armor>();
 
         assert_eq!(item.get_usage(), Usage::MeleeWeapon);
-        assert_eq!(possibly_an_axe.is_some(), true)
+        assert_eq!(possibly_an_axe.is_some(), true);
+        assert_eq!(possibly_an_armor.is_some(), false);
     }
 
     #[test]
     fn it_creates_armor() {
         let item = ItemFactory::create(Item::Armor);
         let possibly_an_armor = item.as_any().downcast_ref::<Armor>();
+        let possibly_a_bow = item.as_any().downcast_ref::<Bow>();
 
         assert_eq!(item.get_usage(), Usage::Protection);
-        assert_eq!(possibly_an_armor.is_some(), true)
+        assert_eq!(possibly_an_armor.is_some(), true);
+        assert_eq!(possibly_a_bow.is_some(), false);
     }
 }
